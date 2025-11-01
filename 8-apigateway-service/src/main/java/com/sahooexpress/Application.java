e0ca30e560379f11f62a57a6cc99f213bd572469
Arunkumar <arunasahoo62@gmail.com>
2025-11-01 18:01:49 +0530

    8-Apigateway_Service

---
+++ 8-apigateway-service/src/main/java/com/sahooexpress/Application.java
@@ -0,0 +1,24 @@
+package com.sahooexpress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import feign.micrometer.MicrometerCapability;
+import io.micrometer.core.instrument.MeterRegistry;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean
+	MicrometerCapability capability(MeterRegistry registry) {
+		return new MicrometerCapability(registry);
+	}
+
+}
